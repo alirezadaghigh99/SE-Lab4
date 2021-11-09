b57d8c01042d095945418c64369bcb334c6d97bd
AlirezaDaqiq <alireza.daghigh1999@gmail.com>
2021-11-09 14:56:26 +0330

    prototype done!

---
+++ Project1/src/main/java/Prototype/Plants/Plant.java
@@ -0,0 +1,33 @@
+package Prototype.Plants;
+
+import java.util.Objects;
+
+public abstract class Plant {
+    public String color;
+    public boolean isLiveInApartment;
+
+    public Plant(Plant target) {
+        if (target != null) {
+            this.color = target.color;
+            this.isLiveInApartment = target.isLiveInApartment;
+        }
+    }
+
+    public Plant() {
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Plant)) {
+            return false;
+        }
+        Plant oC = (Plant) o;
+        return Objects.equals(oC.color, color) && oC.isLiveInApartment == isLiveInApartment;
+    }
+
+    public abstract Plant clone();
+}
